b7acd1570a54ffa362df9a71bda2344911a7a627
danieltonad <soladan2000@gmail.com>
2024-02-03 09:08:54 -0800

    func <=> args rust

--- hello-word/src/main.rs
+++ hello-word/src/main.rs
@@ -1,4 +1,16 @@
 fn main() {
-    let _sum = 2 + 2;
-    println!("Sum: {}", _sum);
+    let sum: i32 = 2;
+    let y: i32 = 0;
+    
+    // {
+    //     let  y: i32 = 200;
+    //     println!("sum = {}, Y = {}", sum, y)
+    // }
+    
+    // println!("sum = {}, Y = {}", sum, y)
+    greet_me("Solarin")
+}
+
+fn greet_me(name: &str){
+    println!("Hello {}", name);
 }

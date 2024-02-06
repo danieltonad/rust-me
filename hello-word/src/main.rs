1cdaa0f9da624186736afc7390ac551b78d24565
danieltonad <soladan2000@gmail.com>
2024-02-06 12:38:27 -0800

    structs

--- hello-word/src/main.rs
+++ hello-word/src/main.rs
@@ -1,26 +1,31 @@
 use std::mem;
 
 fn main() {
+    let o = Object{
+        width: 20,
+        height: 30
+    };
+
+    println!("width: {}, height: {} \n Area: {}", o.width, o.height, area(&o));
     // let sum: i32 = 2;
     // let y: i32 = 0;
     // let mut a : [i32; 10]= [1,2,3,4,5,6,7,8,9,0];
 
     // tuples_example();
 
-    let mut v: Vec<i32> = Vec::new();
+    // let mut v: Vec<i32> = Vec::new();
 
-    for i in 1..1000{
-        v.push(i);   
-    }
-
-    v = re(v);
-    println!("still own v: {} {}", v[0], v[1]);
+    // for i in 1..1000{
+    //     v.push(i);   
+    // }
+    // v = re(v);
+    // println!("still own v: {} {}", v[0], v[1]);
     
-    borrow1(&v);
-    println!("still own v: {} {}", v[0], v[1]);
+    // borrow1(&v);
+    // println!("still own v: {} {}", v[0], v[1]);
 
-    borrow2(&v);
-    println!("still own v: {} {}", v[0], v[1]);
+    // borrow2(&v);
+    // println!("still own v: {} {}", v[0], v[1]);
     
     //     let  y: i32 = 200;
     //     println!("sum = {}, Y = {}", sum, y)
@@ -30,6 +35,9 @@ fn main() {
     // greet_me("Solarin")
 }
 
+fn area(obj: &Object) -> u32 {
+    obj.width * obj.height
+}
 // func args
 fn greet_me(name: &str){
     println!("Hello {}", name);
@@ -76,4 +84,9 @@ fn borrow2(v: &Vec<i32>){
 
 fn count(v: &Vec<i32>, val: i32) -> usize{
     v.into_iter().filter(|&&x| x == val).count()
+}
+
+struct Object {
+    width: u32,
+    height: u32
 }
